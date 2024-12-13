d6f26a083edc4eb76b1c9b0aa66edf9d9f0f0c94
MarioCanudas <mario.cz24@outlook.com>
2024-12-13 17:42:12 -0600

    feat: added method for extract the words with coordinates using ocr

--- Aether_v1/models/document_reader.py
+++ Aether_v1/models/document_reader.py
@@ -1,4 +1,6 @@
 import fitz
+import easyocr as ocr
+import numpy as np
 from core import DocumentReader
 from typing import List, Tuple
 
@@ -19,4 +21,34 @@ class PDFReader(DocumentReader):
                 extracted_words.append(words_from_page)
                 words_from_page = []
                     
+        return extracted_words
+    
+    def extract_words_with_coordinates_by_page(self) -> List[List[Tuple[float, float, str]]]:
+        reader = ocr.Reader(['es', 'en'])
+        
+        words_from_page = []
+        extracted_words = []
+        
+        with fitz.open(self.file_path) as doc:
+            for page in doc:
+                words_from_page = []
+                pix = page.get_pixmap(dpi= 150)
+                
+                img = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.height, pix.width, pix.n)
+                
+                results = reader.readtext(img)
+                
+                for result in results:    
+                    bbox, text, conf = result
+                    
+                    x1, y1 = bbox[0]
+                    x4, y4 = bbox[3]
+                    
+                    x = float((x1 + x4) / 2)
+                    y = float((y1 + y4) / 2)
+                    
+                    words_from_page.append((x, y, text))
+        
+                extracted_words.append(words_from_page)
+                
         return extracted_words
