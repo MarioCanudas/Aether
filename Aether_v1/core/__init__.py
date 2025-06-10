56c39ebd2da72e9b34479cac34c9a9019a6c852f
MarioCanudas <mario.cz24@outlook.com>
2025-06-10 16:40:14 -0600

    refactor: Remove legacy core components

--- Aether_v1/core/__init__.py
+++
@@ -1,6 +0,0 @@
-# core/__init__.py
-from .abstract_base import DocumentReader, BankDetector, TableBoundaryDetector, RowSegmenter, TableReconstructor, TableNormalizer, DataExporter
-
-__all__ = [
-    'DocumentReader', 'BankDetector', 'TableBoundaryDetector', 'RowSegmenter', 'TableReconstructor', 'TableNormalizer', 'DataExporter',
-]
