2d83c620f2cb4598d4d9612e1e4233f33c3d65d7
MarioCanudas <mario.cz24@outlook.com>
2025-07-11 13:45:31 -0600

    feat: Implement date range selection for filtered transactions in data view

--- Aether_v1/frontend/views/data.py
+++ Aether_v1/frontend/views/data.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from controllers.transaction_processor_controller import TransactionProcessorController
+import datetime
+from controllers import DataViewController
 
-controller = TransactionProcessorController()
+controller = DataViewController()
 
 def show_data():
     st.title('Data')
@@ -10,12 +11,28 @@ def show_data():
     if controller.user_have_transactions() and controller.user_have_monthly_results():
         st.write("Data available in Data View")
         
-        df_transactions = controller.get_transactions()
         df_monthly_results = controller.get_monthly_results()
 
         # Display the processed data table or use it for further analysis
         st.header("Transactions")
-        st.dataframe(df_transactions)
+        
+        transactions_date_range = controller.get_transactions_date_range()
+        
+        date_range = st.date_input(
+            label= "Select date range",
+            value= transactions_date_range,
+            min_value= transactions_date_range[0],
+            max_value= transactions_date_range[1],
+            key= "date_range"
+        )
+        
+        try:
+            filtered_transactions = controller.get_filtered_transactions(date_range)
+        except:
+            st.warning("Select a date range to view transactions")
+        finally:
+            if 'filtered_transactions' in locals():
+                st.dataframe(filtered_transactions)
         
         st.header("Monthly Results")
         st.dataframe(df_monthly_results)
