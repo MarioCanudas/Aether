b72488c01631aaefa90ef5e4dd11577a39716202
MarioCanudas <mario.cz24@outlook.com>
2025-07-11 19:34:53 -0600

    feat: Add filtering options for transactions by bank and statement type in data view

--- Aether_v1/controllers/data_view_controller.py
+++ Aether_v1/controllers/data_view_controller.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime, date
+from typing import Optional, Literal, List
 import logging
 from .base_controller import BaseController
 
@@ -57,7 +58,32 @@ class DataViewController(BaseController):
             
             return tuple(date_range)
         
-    def get_filtered_transactions(self, date_range: tuple[date, date]) -> pd.DataFrame:
+    def get_banks_in_transactions(self) -> list[str]:
+        user_id = self.user_session_service.current_user_id
+        
+        query = """
+            SELECT DISTINCT bank FROM transactions
+            WHERE user_id = :user_id
+        """
+        
+        with self.quick_read_scope() as db:
+            result = db.custom_query(
+                query= query,
+                params= {'user_id': user_id},
+                value_format= 'tuple'
+            ) # The result is a list of tuples with the bank name and one None value, ex: [('bank1',), ('bank2',)]
+            
+            banks = [bank[0] for bank in result]
+            
+            return banks
+        
+    def get_filtered_transactions(
+            self, 
+            date_range: tuple[date, date], 
+            banks: Optional[list[str]],
+            statement_type: Optional[Literal['debit', 'credit']],
+            amount_type: Optional[List[Literal['Abono', 'Cargo', 'Saldo inicial']]]
+        ) -> pd.DataFrame:
         user_id = self.user_session_service.current_user_id
         
         first_date, last_date = date_range
@@ -65,17 +91,34 @@ class DataViewController(BaseController):
         last_date = last_date.strftime('%Y-%m-%d')
         
         query = """
-            SELECT * FROM transactions
+            SELECT date, description, amount, type, bank, statement_type, filename FROM transactions
             WHERE user_id = :user_id
             AND date BETWEEN :first_date AND :last_date
         """
         
+        params = {'user_id': user_id, 'first_date': first_date, 'last_date': last_date}
+        
+        if banks:
+            query += f" AND bank IN ({', '.join([f':bank_{i}' for i in range(len(banks))])})"
+            for i, bank in enumerate(banks):
+                params[f'bank_{i}'] = bank
+                
+        if statement_type:
+            query += f" AND statement_type = :statement_type"
+            params['statement_type'] = statement_type
+            
+        if amount_type:
+            query += f" AND type IN ({', '.join([f':type_{i}' for i in range(len(amount_type))])})"
+            for i, amount_type in enumerate(amount_type):
+                params[f'type_{i}'] = amount_type
+                     
         with self.quick_read_scope() as db:
             transactions = db.custom_query(
                 query= query,
-                params= {'user_id': user_id, 'first_date': first_date, 'last_date': last_date},
+                params= params,
                 value_format='dataframe'
             )
+            
             return transactions
         
     def get_monthly_results(self) -> pd.DataFrame:
@@ -84,6 +127,7 @@ class DataViewController(BaseController):
         with self.quick_read_scope() as db:
             monthly_results = db.get_records(
                 table_name='monthly_results',
+                columns= ['year_month', 'initial_balance', 'total_income', 'total_withdrawal', 'savings', 'balance_valid'],
                 where_conditions={'user_id': user_id},
                 value_format='dataframe'
             )
--- Aether_v1/frontend/views/data.py
+++ Aether_v1/frontend/views/data.py
@@ -10,15 +10,30 @@ def show_data():
     # Check if data is available in session state
     if controller.user_have_transactions() and controller.user_have_monthly_results():
         st.write("Data available in Data View")
-        
+        # Get all variables to can filter the transactions
         df_monthly_results = controller.get_monthly_results()
-
-        # Display the processed data table or use it for further analysis
+        transactions_date_range = controller.get_transactions_date_range()
+        banks_in_transactions = controller.get_banks_in_transactions()
+        
+        # Display the transactions table
         st.header("Transactions")
         
-        transactions_date_range = controller.get_transactions_date_range()
+        col1, col2, col3, col4 = st.columns(4)
         
-        date_range = st.date_input(
+
+        statement_type = col1.segmented_control(
+            label= "Select statement type",
+            options= ['debit', 'credit'],
+            key= "statement_type",
+        )
+        
+        amount_type = col2.multiselect(
+            label= "Select amount type",
+            options= ['Abono', 'Cargo', 'Saldo inicial'],
+            key= "amount_type"
+        )
+
+        date_range = col3.date_input(
             label= "Select date range",
             value= transactions_date_range,
             min_value= transactions_date_range[0],
@@ -26,15 +41,21 @@ def show_data():
             key= "date_range"
         )
         
+        banks = col4.multiselect(
+            label= "Select bank",
+            options= banks_in_transactions,
+            key= "banks"
+        )
+
         try:
-            filtered_transactions = controller.get_filtered_transactions(date_range)
+            filtered_transactions = controller.get_filtered_transactions(date_range, banks, statement_type, amount_type)
         except:
             st.warning("Select a date range to view transactions")
+            filtered_transactions = controller.get_filtered_transactions(transactions_date_range, banks, statement_type, amount_type)
         finally:
-            if 'filtered_transactions' in locals():
-                st.dataframe(filtered_transactions)
+            st.dataframe(filtered_transactions, hide_index=True)
         
         st.header("Monthly Results")
-        st.dataframe(df_monthly_results)
+        st.dataframe(df_monthly_results, hide_index=True)
     else:
         st.write("No data available. Please upload files in the Home view.")
