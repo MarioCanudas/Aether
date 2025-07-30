752790cfa8b353802310e310ea8429426e2e6d1d
MarioCanudas <mario.cz24@outlook.com>
2025-07-30 16:58:24 -0600

    chore: Remove database-related constants from configuration files to streamline settings

--- Aether_v1/config/settings.py
+++ Aether_v1/config/settings.py
@@ -7,7 +7,3 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DOCUMENTS_FOLDER = os.path.join(PROJECT_ROOT, 'documents')
 INPUTS_FOLDER = os.path.join(DOCUMENTS_FOLDER, 'inputs')
 OUTPUTS_FOLDER = os.path.join(DOCUMENTS_FOLDER, 'outputs')
-
-# Paths for database
-DATABASE_FOLDER = os.path.join(PROJECT_ROOT, 'database')
-DATABASE_FILE = os.path.join(DATABASE_FOLDER, 'aether.db')
