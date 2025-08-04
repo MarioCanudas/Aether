9bd61c3572406cba07f5e75f9c960eb7311e33a9
MarioCanudas <mario.cz24@outlook.com>
2025-08-04 13:59:56 -0600

    feat: Integrate BaseController for user session management and enhance page navigation structure

--- Aether_v1/frontend/app.py
+++ Aether_v1/frontend/app.py
@@ -14,40 +14,54 @@ logging.basicConfig(
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
-from services.user_session_service import UserSessionService
+from controllers.base_controller import BaseController
+
+# Import views
 from views.home import show_home
 from views.data import show_data
 from views.income_analysis import show_income_analysis
 from views.expenses_analysis import show_expenses_analysis
+from views.transaction_processor import show_transaction_processor  # Import the transaction processor
+
+# Import components
 from components.cash_transaction import adding_cash_transaction
 from components.add_user import add_user_popup
-from views.transaction_processor import show_transaction_processor  # Import the transaction processor
+
+controller = BaseController()
 
 # -- Page Configuration --
 st.logo("frontend/assets/eli-logo.png", size= 'large', icon_image= "frontend/assets/eli-logo.png")
-if not 'all_transactions' in st.session_state:
-    st.session_state.all_transactions = []
     
 # -- Page Navigation --
 PAGES = {
-    'Eli Alpha Version v0.0': [
-        st.Page(show_home, title= "Overview"),
-        st.Page(show_data, title= "Data Export"),
-        st.Page(show_income_analysis, title= "Income Analysis"),
-        st.Page(show_expenses_analysis, title= "Expenses Analysis"),
-        st.Page(show_transaction_processor, title= "Transaction Processor") # New view added here
+    'User': [
+        # st.Page(show_login, title= "Login", icon= ':material/login:'),
+        # st.Page(show_logout, title= "Logout", icon= ':material/logout:'),
+        
+    ],
+    'Overview': [
+        st.Page(show_home, title= "Home", icon= ':material/home:'),
+        st.Page(show_income_analysis, title= "Income Analysis", icon= ':material/trending_up:'),
+        st.Page(show_expenses_analysis, title= "Expenses Analysis", icon= ':material/trending_down:'),
+    ],
+    'Budget': [
+        # st.Page(show_budget, title= "Budget", icon= ':material/budget:'),
+        # st.Page(show_budget_analysis, title= "Budget Analysis", icon= ':material/budget_analysis:'),
+        # st.Page(show_goals, title= "Goals", icon= ':material/goal:'),
+        # st.Page(show_goals_tracker, title= "Goals Tracker", icon= ':material/goal_tracker:'),
+    ],
+    'Data': [
+        st.Page(show_data, title= "Data Export", icon= ':material/table:'),
+        st.Page(show_transaction_processor, title= "Transaction Processor", icon= ':material/flowsheet:'),
     ],
 }
 
-user_session_service = UserSessionService()
-users = user_session_service.get_available_users()
-
 # -- Sidebar configuration --
 with st.sidebar:
-    current_username = st.selectbox("Select User", users, index= None)
+    current_username = st.selectbox("Select User", controller.get_users(), index= None)
     
     if st.button('Add user'):
-        add_user_popup(user_session_service)
+        add_user_popup()
     
     # Disable adding cash transaction button, because it's not implemented properly yet
     if st.button('Add cash transaction', type= 'primary'):
@@ -55,12 +69,17 @@ with st.sidebar:
 
 # -- User session management --
 if current_username is None:
-    user_session_service.clear_current_user()
+    controller.clear_user_session()
     st.toast("No user selected")
-elif current_username is not None and not user_session_service.current_user_id:
-    user_id = user_session_service.get_user_id_by_username(current_username)
-    user_session_service.set_current_user_by_id(user_id)
-    st.toast(f"Logged in as {current_username} with id {user_session_service.current_user_id}")
+    
+elif current_username is not None:
+    if not controller.user_id:
+        user_id = controller.get_user_id(current_username)
+        controller.update_user_id(user_id)
+        
+        st.toast(f"Logged in as {current_username} with id {controller.user_id}")
+    else:
+        st.toast(f"Already logged in as {current_username} with id {controller.user_id}")
 
         
 page = st.navigation(PAGES)
