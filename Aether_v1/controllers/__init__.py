31805aee03319280b22063f0ad7dc4f1c81796da
MarioCanudas <mario.cz24@outlook.com>
2025-08-07 12:27:47 -0600

    feat: Enhance user session management and frontend file upload functionality

--- Aether_v1/controllers/__init__.py
+++ Aether_v1/controllers/__init__.py
@@ -1,9 +1,15 @@
+from .logs_controller import LogsController
+from .user_configuration_controller import UserConfigurationController
 from .transaction_processor_controller import TransactionProcessorController
 from .analysis_controller import AnalysisController
 from .data_view_controller import DataViewController
+from .budget_controller import BudgetController
 
 __all__ = [
+    "LogsController",
+    "UserConfigurationController",
     "TransactionProcessorController",
     "AnalysisController",
-    "DataViewController"
+    "DataViewController",
+    "BudgetController"
 ]
