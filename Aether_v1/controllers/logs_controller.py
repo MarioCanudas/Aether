fcba858fe9112008a459bfaedac7b3bcbf5fe9f6
MarioCanudas <mario.cz24@outlook.com>
2025-08-12 11:03:49 -0600

    feat: Add user retrieval methods in LogsController to enhance user management functionality

--- Aether_v1/controllers/logs_controller.py
+++ Aether_v1/controllers/logs_controller.py
@@ -1,3 +1,5 @@
+from typing import List
+from services import UserDBService
 from .base_controller import BaseController
 
 class LogsController(BaseController):
@@ -6,3 +8,16 @@ class LogsController(BaseController):
     
     def clear_user_session(self) -> None:
         self.user_session_service.clear_current_user()
+        
+    def get_users(self) -> List[str]:
+        with self.quick_read_conn() as conn:
+            users_db = UserDBService(conn)
+            
+            return users_db.get_unique_values('username')
+        
+    def get_user_id(self, username: str) -> int:
+        with self.quick_read_conn() as conn:
+            users_db = UserDBService(conn)
+            
+            return users_db.find_id(username= username)
+        
