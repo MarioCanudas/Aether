af9699aedb9cc4d1608c0392c46cec4457f2e762
MarioCanudas <mario.cz24@outlook.com>
2025-09-04 16:10:25 -0600

    feat: Add new template and modify template dialogs for transaction management

--- Aether_v1/frontend/components/__init__.py
+++ Aether_v1/frontend/components/__init__.py
@@ -1,5 +1,9 @@
 from .confirm_upload import confirm_upload_popup
+from .new_template import new_template_popup
+from .modify_template import modify_template_popup
 
 __all__ = [
-    'confirm_upload_popup'
+    'confirm_upload_popup',
+    'new_template_popup',
+    'modify_template_popup'
 ]
