e517fde42bb3bc0e2ab2415926ca28caa37ac2b5
MarioCanudas <mario.cz24@outlook.com>
2025-09-20 14:02:31 -0600

    feat: Enhance data management by adding data_editor in transactions table view

--- Aether_v1/controllers/data_view_controller.py
+++ Aether_v1/controllers/data_view_controller.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from datetime import date
-from typing import Optional, Literal, List, Tuple
+from typing import Optional, Literal, List, Dict
 import logging
-from services import TransactionsDBService
+from services import TransactionsDBService, CategoryDBService
+from models.amounts import TransactionType
+from models.bank_properties import StatementType, BankName
+from models.dates import Period
+from models.records import TransactionRecord
 from .base_controller import BaseController
 
 logger = logging.getLogger(__name__)
 
 class DataViewController(BaseController):
-    def get_transactions_date_range(self) -> Tuple[date, date]:
+    def get_transactions_date_range(self) -> Period:
         user_id = self.user_session_service.current_user_id
         
         with self.quick_read_conn() as conn:
@@ -16,7 +19,7 @@ class DataViewController(BaseController):
             
             period = transactions_db.get_transactions_period(user_id)
             
-            return period.to_tuple()
+            return period
         
     def get_banks_in_transactions(self) -> List[str]:
         user_id = self.user_session_service.current_user_id
@@ -28,10 +31,10 @@ class DataViewController(BaseController):
         
     def get_filtered_transactions(
             self, 
-            date_range: Tuple[date, date], 
-            banks: Optional[list[str]],
-            statement_type: Optional[Literal['debit', 'credit']],
-            amount_type: Optional[List[Literal['Abono', 'Cargo', 'Saldo inicial']]]
+            period: Period, 
+            banks: Optional[list[BankName]],
+            statement_type: Optional[StatementType],
+            amount_type: Optional[List[TransactionType]]
         ) -> pd.DataFrame:
         user_id = self.user_session_service.current_user_id
         
@@ -40,15 +43,82 @@ class DataViewController(BaseController):
             
             transactions = transactions_db.get_transactions(
                 user_id= user_id,
-                columns= ['date', 'description', 'amount', 'type', 'bank', 'statement_type', 'filename'],
-                period= date_range,
+                columns= ['transaction_id', 'date', 'description', 'amount', 'type', 'bank', 'statement_type', 'filename'],
+                period= period,
                 banks= banks,
-                statement_type= statement_type,
-                amount_type= amount_type,
+                statement_type= statement_type.value,
+                amount_type= amount_type.value,
                 show_categories_names= True
             )
             
-            df = pd.DataFrame(transactions)
+            return pd.DataFrame(transactions)
+        
+    def get_categories(self, mapped: bool = False) -> List[str] | Dict[str, int]:        
+        with self.quick_read_conn() as conn:
+            categories_db = CategoryDBService(conn)
             
-            # Set the order of the columns
-            return df[['date', 'category', 'description', 'amount', 'type', 'bank', 'statement_type', 'filename']]
+            if mapped:
+                return categories_db.get_categories_by_user_mapped(self.user_id) + {None: None}
+            else:
+                return categories_db.get_categories_by_user(self.user_id)
+                
+    @staticmethod
+    def _extract_modified_transactions(original_transactions: pd.DataFrame, edited_transactions: pd.DataFrame) -> List[TransactionRecord]:
+        if not original_transactions.equals(edited_transactions):
+            df_og = original_transactions.copy()
+            df_ed = edited_transactions.copy()
+            
+            df_og_tuples: pd.Series = df_og.apply(tuple, axis= 1)
+            df_ed_tuples: pd.Series = df_ed.apply(tuple, axis= 1)
+            
+            mask = ~df_ed_tuples.isin(df_og_tuples)
+            
+            return df_ed[mask].to_dict(orient= 'records')
+        else:
+            return []
+        
+    @staticmethod
+    def _classify_modified_transactions(original_ids: pd.Series, modified_transactions: List[TransactionRecord]) -> Dict[Literal['new', 'modified', 'deleted'], List[TransactionRecord]]:
+        new_transactions = []
+        modified_transactions_list = []
+        deleted_transactions = []
+        
+        last_original_id = original_ids.max()
+        
+        for transaction in modified_transactions:
+            if transaction['transaction_id'] > last_original_id:
+                new_transactions.append(transaction)
+            elif transaction['transaction_id'] in original_ids:
+                modified_transactions_list.append(transaction)
+            
+                
+        return {'new': new_transactions, 'modified': modified_transactions_list, 'deleted': deleted_transactions}
+        
+    def modify_transactions(self, original_transactions: pd.DataFrame, edited_transactions: pd.DataFrame) -> None:
+        modified_transactions = self._extract_modified_transactions(original_transactions, edited_transactions)
+        
+        if not modified_transactions:
+            return
+        
+        # Chanche the categorie (str) to category_id (int)
+        categories = self.get_categories(mapped= True)
+        for transaction in modified_transactions:
+            transaction['category_id'] = categories[transaction['category']]
+            transaction['user_id'] = self.user_id
+            del transaction['category']
+            
+        classified_transactions = self._classify_modified_transactions(original_transactions['transaction_id'], modified_transactions)
+        
+        with self.batch_conn() as conn:
+            transactions_db = TransactionsDBService(conn)
+            
+            if classified_transactions['new']:
+                transactions_db.add_records(classified_transactions['new'])
+                
+            if classified_transactions['modified']:
+                transactions_db.update_transactions(classified_transactions['modified'])
+                
+            if classified_transactions['deleted']:
+                transactions_db.delete_records(classified_transactions['deleted'])
+                
+                
--- Aether_v1/frontend/views/data.py
+++ Aether_v1/frontend/views/data.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from controllers import DataViewController
+from models.amounts import TransactionType
+from models.bank_properties import BankName, StatementType
+from models.dates import Period
 
 controller = DataViewController()
 
@@ -9,7 +12,7 @@ def show_data():
     # Check if data is available in session state
     if controller.user_have_transactions():
         # Get all variables to can filter the transactions
-        transactions_date_range = controller.get_transactions_date_range()
+        transactions_period = controller.get_transactions_date_range()
         banks_in_transactions = controller.get_banks_in_transactions()
         
         col1, col2, col3, col4 = st.columns(4)
@@ -17,22 +20,20 @@ def show_data():
 
         statement_type = col1.segmented_control(
             label= "Select statement type",
-            options= ['debit', 'credit'],
+            options= [statement_type.value for statement_type in StatementType],
             key= "statement_type",
         )
         
-        amount_type = col2.multiselect(
+        amount_types = col2.multiselect(
             label= "Select amount type",
-            options= ['Abono', 'Cargo', 'Saldo inicial'],
+            options= [tt.value for tt in TransactionType],
             key= "amount_type"
         )
 
         date_range = col3.date_input(
             label= "Select date range",
-            value= transactions_date_range,
-            min_value= transactions_date_range[0],
-            max_value= transactions_date_range[1],
-            key= "date_range"
+            value= transactions_period.to_tuple(),
+            key= "date_range",
         )
         
         banks = col4.multiselect(
@@ -40,13 +41,84 @@ def show_data():
             options= banks_in_transactions,
             key= "banks"
         )
+        
+        if statement_type:
+            statement_type = StatementType(statement_type)
+            
+        if amount_types:
+            amount_types = [TransactionType(amount_type) for amount_type in amount_types]
+            
+        if date_range:
+            period = Period(date_range[0], date_range[1])
+            
+        if banks:
+            banks = [BankName(bank) for bank in banks]
 
         try:
-            filtered_transactions = controller.get_filtered_transactions(date_range, banks, statement_type, amount_type)
+            filtered_transactions = controller.get_filtered_transactions(period, banks, statement_type, amount_types)
         except Exception:
-            filtered_transactions = controller.get_filtered_transactions(transactions_date_range, banks, statement_type, amount_type)
+            filtered_transactions = controller.get_filtered_transactions(transactions_period, banks, statement_type, amount_types)
         finally:
-            st.dataframe(filtered_transactions, hide_index=True)
+            edited_transactions = st.data_editor(
+                data= filtered_transactions.copy(),
+                column_config=  {
+                    'date' : st.column_config.DateColumn(
+                        label= 'Date',
+                        format= 'YYYY/MM/DD'
+                    ),
+                    'category' : st.column_config.SelectboxColumn(
+                        label= 'Category',
+                        options= controller.get_categories() + [None]
+                    ),
+                    'description' : st.column_config.TextColumn(
+                        label= 'Description',
+                        max_chars= 200,
+                    ),
+                    'amount' : st.column_config.NumberColumn(
+                        label='Amount',
+                        format='$ %.2f',
+                        min_value= 0.01,
+                        required= True
+                    ),
+                    'type' : st.column_config.SelectboxColumn(
+                        label= 'Type',
+                        options= [tt.value for tt in TransactionType],
+                        required= True
+                    ),
+                    'bank' : st.column_config.SelectboxColumn(
+                        label= 'Bank',
+                        options= [bn.value for bn in BankName],
+                        required= True
+                    ),
+                    'statement_type' : st.column_config.SelectboxColumn(
+                        label= 'Statement Type',
+                        options= [statement_type.value for statement_type in StatementType],
+                        required= True
+                    ),
+                    'filename' : st.column_config.TextColumn(
+                        label= 'Filename',
+                        max_chars= 50,
+                    )
+                },
+                column_order= ['date', 'category', 'description', 'amount', 'type', 'bank', 'statement_type', 'filename'],
+                use_container_width= True,
+                hide_index= True,
+                width = 'stretch',
+                num_rows= 'dynamic',
+            )
+        
+        if st.button(label= '', icon= ':material/save:', help= 'Save transactions'):
+            if not filtered_transactions.equals(edited_transactions):
+                
+                controller.modify_transactions(
+                    original_transactions= filtered_transactions, 
+                    edited_transactions= edited_transactions
+                )
+                st.toast('Transactions updated successfully', icon= ':material/check:')
+                st.rerun()
+            else:
+                st.toast('No changes to save', icon= ':material/info:')
+            
     else:
         st.write("No transactions available. Please upload files or input transactions manually.")
         
