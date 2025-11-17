6f0a35ac64c9ca053492016c571e017baeccbb56
MarioCanudas <mario.cz24@outlook.com>
2025-11-16 21:01:05 -0600

    feat: Add Cards view and new card popup for managing card data in the application

--- Aether_v1/frontend/components/__init__.py
+++ Aether_v1/frontend/components/__init__.py
@@ -7,6 +7,7 @@ from .period_select_box import period_select_box
 from .new_category import new_category_popup
 from .modify_profile_popup import modify_profile_popup
 from .add_user_popup import add_user_popup
+from .new_card import new_card_popup
 
 __all__ = [
     'confirm_upload_popup',
@@ -19,4 +20,5 @@ __all__ = [
     'new_category_popup',
     'modify_profile_popup',
     'add_user_popup',
+    'new_card_popup',
 ]
---
+++ Aether_v1/frontend/components/new_card.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from controllers import CardsViewController
+from models.bank_properties import BankName, StatementType
+from models.cards import Card
+
+@st.dialog('New Card')
+def new_card_popup():
+    controller = CardsViewController()
+    
+    with st.form(key= 'new_card_form', border= False, clear_on_submit= True):
+        card_name = st.text_input('Name', value= None, key= 'new_card_name')
+        
+        left, right = st.columns(2)
+        card_bank = left.selectbox('Bank', options= BankName.get_values(), index= None, key= 'new_card_bank')
+        card_statement_type = right.selectbox('Statement Type', options= StatementType.get_values(), index= None, key= 'new_card_statement_type')
+        
+        card_expiration_date = st.date_input('Expiration Date', value= None, key= 'new_card_expiration_date')
+        
+        if st.form_submit_button('Add Card', type= 'primary'):
+            controller.add_card(card_name, BankName(card_bank), StatementType(card_statement_type), card_expiration_date)
+            st.rerun()
