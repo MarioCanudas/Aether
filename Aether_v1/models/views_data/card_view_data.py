c4c6ba406e34cfdc41e2779c68aac00bfd94dea0
MarioCanudas <mario.cz24@outlook.com>
2025-11-17 20:10:41 -0600

    refactor: Update CardViewData to use dictionary for transactions and adjust method return types

--- Aether_v1/models/views_data/card_view_data.py
+++ Aether_v1/models/views_data/card_view_data.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel, ConfigDict, field_validator
 import pandas as pd
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 from altair import Chart
 from ..cards import CardMetrics
-from ..records import TransactionRecord
 
 class CardViewData(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
     
     metrics: CardMetrics
-    transactions: List[TransactionRecord]
+    transactions: List[Dict[str, Any]]
     income_vs_expenses_chart: Optional[Chart] = None
     
     @field_validator('income_vs_expenses_chart')
@@ -20,7 +19,7 @@ class CardViewData(BaseModel):
         
         return income_vs_expenses_chart
     
-    def get_last_transactions(self, in_df: bool = False) -> List[TransactionRecord] | pd.DataFrame:
+    def get_last_transactions(self, in_df: bool = False) -> List[Dict[str, Any]] | pd.DataFrame:
         if in_df:
             return pd.DataFrame(self.transactions[:5])
         else:
