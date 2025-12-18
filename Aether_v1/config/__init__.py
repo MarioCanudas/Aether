5ac8d48fc04cfd1d4ecd57dbc1949a5101b94e30
MarioCanudas <mario.cz24@outlook.com>
2025-12-17 20:06:48 -0600

    refactor: Apply ruff format into config files

--- Aether_v1/config/__init__.py
+++ Aether_v1/config/__init__.py
@@ -1,15 +1,15 @@
 # config/__init__.py
 
 from .settings import (
-    PROJECT_ROOT,
     DOCUMENTS_FOLDER,
     INPUTS_FOLDER,
     OUTPUTS_FOLDER,
+    PROJECT_ROOT,
 )
 
 __all__ = [
-    'PROJECT_ROOT', 
-    'DOCUMENTS_FOLDER', 
-    'INPUTS_FOLDER', 
-    'OUTPUTS_FOLDER', 
-    ]
+    "PROJECT_ROOT",
+    "DOCUMENTS_FOLDER",
+    "INPUTS_FOLDER",
+    "OUTPUTS_FOLDER",
+]
--- Aether_v1/config/settings.py
+++ Aether_v1/config/settings.py
@@ -4,6 +4,6 @@ import os
 PROJECT_ROOT: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Paths for documents
-DOCUMENTS_FOLDER: str = os.path.join(PROJECT_ROOT, 'documents')
-INPUTS_FOLDER: str = os.path.join(DOCUMENTS_FOLDER, 'inputs')
-OUTPUTS_FOLDER: str = os.path.join(DOCUMENTS_FOLDER, 'outputs')
+DOCUMENTS_FOLDER: str = os.path.join(PROJECT_ROOT, "documents")
+INPUTS_FOLDER: str = os.path.join(DOCUMENTS_FOLDER, "inputs")
+OUTPUTS_FOLDER: str = os.path.join(DOCUMENTS_FOLDER, "outputs")
